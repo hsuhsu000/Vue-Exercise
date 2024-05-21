de44d7cd26acb48b597ed15742be23b308508e08
hsuhsu000 <hsushwesinoo2002@gmail.com>
2024-05-21 18:45:36 +0800

    mapstate and mapgetters finish

--- vue_x_2_3_test/src/store/index.ts
+++ vue_x_2_3_test/src/store/index.ts
@@ -1,14 +1,22 @@
-import { createStore } from 'vuex'
+import { createStore } from "vuex";
 
 export default createStore({
   state: {
+    name: "Hsu",
+    nickname: "Papi",
+    fruits: ["apple", "orange", "mango"],
+    users: [
+      { id: 1, name: "Hsu", isDone: false },
+      { id: 2, name: "Papi", isDone: true },
+    ],
   },
   getters: {
+    getUserCount: (state) => state.users.length,
+    doneJob: (state) => state.users.filter((v) => v.isDone === true),
+    undoneJob: (state, getters) =>
+      getters.getUserCount - getters.doneJob.length,
   },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
-})
+  mutations: {},
+  actions: {},
+  modules: {},
+});
--- vue_x_2_3_test/src/views/HomeView.vue
+++ vue_x_2_3_test/src/views/HomeView.vue
@@ -1,9 +1,43 @@
 <template>
-  <div><h1>Home Page</h1></div>
+  <div>
+    <h1>Home Page</h1>
+    <h1>{{ $store.state.name }}</h1>
+    <button class="btn btn-primary" @click="showStateName()">
+      Show State Name
+    </button>
+    <h1>Fruits in State</h1>
+    <li v-for="(f, index) in myFruits" :key="index">{{ f }}</li>
+    <h1>{{ getMyName }}</h1>
+    <!-- <h1>Getters {{ $store.getters.getUserCount }}</h1>
+    <h1>{{ $store.getters.doneJob[0].name }}</h1>
+    <h1>{{ $store.getters.undoneJob }}</h1> -->
+    <h1>{{ getUserCount }}</h1>
+    <h1>{{ doneJob[0].name }}</h1>
+    <h1>{{ undoneJob }}</h1>
+  </div>
 </template>
 
 <script>
-export default {};
+import { mapState, mapGetters } from "vuex";
+export default {
+  data() {
+    return {
+      myFruits: this.$store.state.fruits,
+    };
+  },
+  computed: {
+    ...mapState(["name", "nickname"]),
+    ...mapGetters(["getUserCount", "doneJob", "undoneJob"]),
+    getMyName() {
+      return this.name + " (" + this.nickname + ")";
+    },
+  },
+  methods: {
+    showStateName() {
+      alert(this.$store.state.name);
+    },
+  },
+};
 </script>
 
 <style></style>
